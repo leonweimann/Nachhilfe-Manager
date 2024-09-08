333076ae9ad89b4a77f13523d0f2d0185f56f337
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-09-08 16:18:59 +0200

    Initial Commit

---
+++ Nachhilfe Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nachhilfe Manager
+//
+//  Created by Leon Weimann on 08.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nachhilfe Manager/Nachhilfe_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nachhilfe_ManagerApp.swift
+//  Nachhilfe Manager
+//
+//  Created by Leon Weimann on 08.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Nachhilfe_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
