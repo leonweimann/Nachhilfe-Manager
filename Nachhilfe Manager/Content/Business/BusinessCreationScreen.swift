41f5c44d9fb62cc04bfb09f85c8d734376e0debd
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-09-26 21:03:37 +0200

    Added business name screen to BusinessCreationScreen

--- Nachhilfe Manager/Content/Business/BusinessCreationScreen.swift
+++ Nachhilfe Manager/Content/Business/BusinessCreationScreen.swift
@@ -14,26 +14,63 @@ struct BusinessCreationScreen: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "plus")
+            Image(systemName: "storefront")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.tint.secondary)
+                .frame(width: 200, height: 200)
+                .padding(32)
             
-            Text("Description")
+            Text("Choose the name of your business")
+                .foregroundStyle(.secondary)
             
-            Text("TextField title")
+            Divider()
+                .padding(.vertical)
             
-            TextField("Your business name", text: $business.name)
+            TextField("Example Business", text: $business.name)
+                .textFieldStyle(.roundedBorder)
+                .autocapitalization(.words)
+                .textContentType(.organizationName)
+                .padding(24)
             
-            Button {
-                
+            Spacer()
+            
+            NavigationLink {
+                contactInformationScreen
             } label: {
                 Label("Confirm", systemImage: "chevron.forward")
+                    .font(.headline)
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 4)
             }
+            .buttonStyle(.borderedProminent)
+            .padding(.vertical, 32)
+            .disabled(business.name.count < 3)
             
-            Text("You will be able to change this etc.")
+            Divider()
+                .padding(.vertical)
+            
+            Text("The name is crucial for your business to be recognized. You will be able to change it later.")
+                .foregroundStyle(.secondary)
+                .font(.footnote)
         }
+        .multilineTextAlignment(.center)
+        .padding()
         .interactiveDismissDisabled()
+    }
+    
+    // MARK: -
+    
+    private var contactInformationScreen: some View {
+        ContentUnavailableView("Contact Information", systemImage: "person.crop.circle.fill.badge.plus")
+            .navigationTitle(business.name)
     }
 }
 
 #Preview {
-    BusinessCreationScreen()
+    NavigationStack {
+        BusinessCreationScreen()
+            .navigationTitle("this is empty")
+            .navigationBarTitleDisplayMode(.inline)
+    }
 }
